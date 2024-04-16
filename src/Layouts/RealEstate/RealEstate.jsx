57d48921afe4f8c9b338937cb2894705fb13de77
AsaduzzamanSohun <asaduzzamansohun5@gmail.com>
2024-04-16 11:28:55 +0600

    proptypes error solved

--- src/Layouts/RealEstate/RealEstate.jsx
+++ src/Layouts/RealEstate/RealEstate.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 
 const RealEstate = ({realEstate}) => {
 
@@ -11,24 +13,24 @@ const RealEstate = ({realEstate}) => {
 
             <div className='bg-slate-50'>
                 <div className='p-4'>
-                    <h3 className='text-sky-500 font-semibold text-xl'>$ 6728</h3>
-                    <h2 className='text-2xl font-semibold my-2'>Apartment</h2>
+                    <h3 className='text-sky-500 font-semibold text-xl'>{price}</h3>
+                    <h2 className='text-2xl font-semibold my-2'>{estate_title}</h2>
 
-                    <p className='font-thin'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem odio quo quam voluptas tempora repudiandae sint voluptates saepe porro ab.</p>
+                    <p className='font-thin'>{description}</p>
 
                 </div>
 
                 <div className='flex justify-between items-center border-y border-zinc-800 p-4 font-medium'>
-                    <div><span>Apartment</span></div>
-                    <div><span>rent</span></div>
-                    <div><span>1200 sq ft</span></div>
+                    <div><span>{segment_name}</span></div>
+                    <div><span>{status}</span></div>
+                    <div><span>{area}</span></div>
                 </div>
 
             </div>
 
 
             <div className='flex justify-between px-4 py-2'>
-                <span>location</span>
+                <span>{location}</span>
                 <button className='font-light bg-stone-500 hover:text-blue-700 hover:font-bold px-3 text-zinc-100 animate__animated animate__pulse animate__infinite animate__slow hover:skeleton duration-500 transition-all'>{`see details ->`}</button>
             </div>
 
@@ -37,3 +39,7 @@ const RealEstate = ({realEstate}) => {
 };
 
 export default RealEstate;
+
+RealEstate.propTypes = {
+    realEstate: PropTypes.object
+}
